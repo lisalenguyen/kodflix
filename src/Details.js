1ee485d34dcb3405189639253f450d1c462875e4
lisalenguyen <57147650+lisalenguyen@users.noreply.github.com>
2019-11-19 15:59:31 +0000

    #14 TV Show Title on Details Page (#1)

--- src/Details.js
+++ src/Details.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import getCountries from './countries-get'
 
 export default class Details extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            welcomeMessage: 'Welcome to the details page, WIP :)'
+            country: {}
         };
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                welcomeMessage: 'The best is yet to come!'
-            });
-        }, 3000);
+        let countryId = this.props.match.params.countryId;
+        let country = getCountries()
+            .find((country) => country.id === countryId);
+        this.setState({ country });
     }
 
     render() {
         return (
-        <div><h1>{this.state.welcomeMessage}</h1>
-            <Link to='/'>Back to home page</Link>
-        </div>
+            <div><h1>{this.state.country.name}</h1>
+                <Link to='/'>Back to home page</Link>
+            </div>
         );
     }
 }
